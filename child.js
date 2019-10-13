3ea42b8cd3b6da392b5a0bbc78bd639bb0c19c15
he135 <33375623+he135@users.noreply.github.com>
2019-10-13 00:08:05 -0400

    Add files via upload

---
+++ child.js
@@ -0,0 +1,23 @@
+localStorage.setItem("lastname", "Jackson!!");
+console.log("AELKLSJFAKLSDJFLKSJFDALSKFJD");
+
+// window.onload = function() { // dealing w async honestly sucks
+//     var data = document.getElementById('data');
+//     var output = document.getElementById('event-data');
+
+//     function displayStorageEvent(e) {
+//         if (e.key == 'storage-event') {
+//             print("sdfljsldkfjsldkfj")
+//             output.innerHTML = e.newValue;
+//         }
+//     }
+
+//     function updateStorageEvent() {
+//         localStorage.setItem('storage-event', this.value);
+//     }
+
+//     window.addEventListener("storage", displayStorageEvent, true);
+//     data.addEventListener("keyup", updateStorageEvent, true);
+// }
+
+// // https://stackoverflow.com/questions/26107125/cannot-read-property-addeventlistener-of-null
