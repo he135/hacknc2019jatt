9f38e926b34d65eddae26243d7ef94af8db6c14d
he135 <thereseym@gmail.com>
2019-10-13 01:02:20 -0400

    parent started; gitignore

--- parent.js
+++ parent.js
@@ -1,1 +1,17 @@
-// console.log(localStorage.getItem("lastname"));
+// var data = console.log(localStorage.getItem("lastname"));
+
+// Hardcoded data array:
+var data = ["Dishes task in progress.", "Dishes task completed.",
+    "Laundry task in progress.", "Mowing task in progress.",
+    "Laundry task completed."
+];
+
+window.onload = function() {
+    for (var i = 0; i < data.length; i++) {
+        // console.log(data[i]);
+        var node = document.createElement("DIV");
+        var textnode = document.createTextNode(data[i]);
+        node.appendChild(textnode);
+        document.getElementById("content").appendChild(node);
+    }
+}
