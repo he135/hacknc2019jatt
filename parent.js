bcad0e542e3c358b1341b35e661b0c92651af1e0
he135 <thereseym@gmail.com>
2020-01-06 13:44:41 -0500

    updated parent view

--- parent.js
+++ parent.js
@@ -11,7 +11,12 @@ window.onload = function() {
         // console.log(data[i]);
         var node = document.createElement("DIV");
         var textnode = document.createTextNode(data[i]);
-        node.className = "row tasks";
+        node.className = "row tasks border border-success";
+        if (data[i].includes("completed")) { // ecma 6 only
+            node.className = "row tasks border border-success completed";
+        } else { // in progress
+            node.className = "row tasks border border-warning in-progress";
+        }
         node.appendChild(textnode);
         document.getElementById("content").appendChild(node);
     }
